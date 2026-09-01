b9ca49bbe67ad4ba41ac61feca11f69cb3502a37
Your Name <miftamiftuu23@gmail.com>
2026-09-01 22:47:51 +0600

    dt and comments

---
+++ DT.py
@@ -0,0 +1,32 @@
+"""
+data types
+#Basic types(integer(any number),float,complex,boolean,spring)
+"""
+#container types-(list,tuples.sets and dictionary)
+#user-defined types-class
+#integer(any number)
+print(4000000000000000000000000000)
+print(1e308)
+print(1e309)
+
+#float
+print(4.5)
+#boolean
+print(True)
+print(False)
+#complex
+print(4+5j)
+#string
+print('mifta')
+
+
+#container types
+
+#list
+print([1,2,3])
+#tuple
+print((1,2,3,4))
+#sets
+print({1,2,3,4})
+#dic
+print({'name':"mifta",'age':30})
--- hello.py
+++ hello.py
@@ -1,3 +1,13 @@
-print("Hello Mifta!")
-
-print("Python is working.")
+name="Mifta"
+age=21
+price=23.54
+print("hello world")
+print(5.6)
+print(name)
+print(age)
+print(name,age,price)
+print(name,age,price,sep='/')
+print(name,age,price,sep='-')
+print(name,age,price,sep='/n')
+print("hello",end='n')
+print("world")
