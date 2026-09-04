b51b312057d4fc6672c55390b2dac1bce109e60f
Your Name <miftamiftuu23@gmail.com>
2026-09-04 22:32:50 +0600

    variable

---
+++ variable.py
@@ -0,0 +1,14 @@
+#variable declare
+name='mifta'
+print(name)
+#dynamic typing:python er moddhe variable create korle automatic bujhe jai j variable er data type ki hoi
+#static typing:python er moddhe hoina.ekhane datatype likha lage
+#dynamic binding:multiple dt store korte para
+#special syntex
+a=5;
+b=6;
+c=7;
+print(a,b,c)
+
+
+#keyword and identifiers
